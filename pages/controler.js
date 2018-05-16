92ec202660d8cb3d9c708c43377e85775f17b939
Patryk Olechno <patrykoolechno@gmail.com>
2018-05-16 16:11:49 +0200

    Some bugs

--- pages/controler.js
+++ pages/controler.js
@@ -2,6 +2,10 @@ module.exports = function(app,bodyParser,mongo){
 	let urlEncoder = bodyParser.urlencoded({ extended: false })
 	let err=[];
 
+	app.get('/',function(req,res){
+		res.render('home',{error: null});
+	});
+
 	app.get('/login',function(req,res){
 		res.render('login',{error: null});
 	});
@@ -23,11 +27,11 @@ module.exports = function(app,bodyParser,mongo){
 
 		let errors = req.validationErrors();
 		if(errors){
-			
+
 			errors.forEach(function(element,index,array){
 				err.push(element.msg)
 			})
-			
+
 			res.render('register',{err})
 			err = [];
 		}else{
@@ -35,5 +39,5 @@ module.exports = function(app,bodyParser,mongo){
 			mongo.createUser(req.body.login,req.body.email,req.body.pass)
 		}
 	})
-	
+
 };
