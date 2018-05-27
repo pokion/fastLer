0cd6acdd9c7ad80505ff9418a126224fd24fabb5
Czarek <pokion@outlook.com>
2018-05-27 12:01:21 +0200

    login rejestracja button

--- pages/controler.js
+++ pages/controler.js
@@ -6,10 +6,6 @@ module.exports = function(app,bodyParser,mongo,mongoose){
 		res.render('login',{error: null});
 	});
 
-	app.get('*', function(req, res){
-	 	res.send("błąd 404",404)
-	});
-
 	app.get('/login',function(req,res){
 		res.render('login',{error: null});
 	});
